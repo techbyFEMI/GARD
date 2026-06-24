fa5233d48b6219adfdc2b977803ee57f1f6d6efd
FEMI <techbyfemi@gmail.com>
2026-06-24 20:51:15 +0100

    Update CORS middleware to allow specific origins

--- Core.py
+++ Core.py
@@ -2,7 +2,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.middleware(
   CORSMiddleware,
-  allow_origin=["*"],
+  settings.add_origin,
+  "https://gardfrontend.vercel.app"
+  "https://gardfrontend.vercel.app/index2.html"
   allow_methods=["*"],
   allow_headers=["*"]
 )
