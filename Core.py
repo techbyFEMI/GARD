9fa8f0ed0fd796d60bc90cc8731981f53a8f47d4
FEMI <techbyfemi@gmail.com>
2026-06-24 20:55:34 +0100

    Fix middleware configuration syntax in Core.py

--- Core.py
+++ Core.py
@@ -1,11 +1,11 @@
 from fastapi.middleware.cors import CORSMiddleware
 
-app.middleware(
+app.add_middleware(
   CORSMiddleware,
   allow_origins=[
   "https://gardfrontend.vercel.app"
   "https://gardfrontend.vercel.app/index2.html"
-  ]
+  ],
   allow_methods=["*"],
   allow_headers=["*"]
 )
